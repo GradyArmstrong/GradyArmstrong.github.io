35e32fef15c921afab80a100a6a89d84883c4c96
GradyArmstrong <144718427+GradyArmstrong@users.noreply.github.com>
2024-04-24 18:38:25 +0000

    task 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 console.log("Task 1")
 for (i=0; i < 11; i++){ console.log(i*i)}
 console.log("Task 2")
-
+for (let i=5; i >= 1; i--)
+{console.log(i)}
+console.log("Blastoff!");
 console.log("Task 3")
 
 console.log("Task 4")
