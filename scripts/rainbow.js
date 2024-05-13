4108500cf4bbbf9a2ffe21e32d6426c2c04ba3b1
GradyArmstrong <144718427+GradyArmstrong@users.noreply.github.com>
2024-05-13 16:41:32 +0000

    rainbow

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,9 +1,12 @@
 let arrcolor = ["red","orange","yellow","green","blue","purple","pink"]
-let colorlist = document.createElement('ul');
+
+let ul = document.createElement('ul');
+
 for (let i = 0; i < arrcolor.length; i++){
-    let itemcolor = document.createElement('li');
+    let li = document.createElement('li');
     itemcolor.innerText = arrcolor[i];
     itemcolor.style.color = arrcolor[i];
-    colorlist.appendChild(itemcolor);
+    colorlist.appendChild(li);
 }
-document.body.appendChild(colorlist);
+
+document.body.appendChild(ul);
