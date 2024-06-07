47be4142d730d9119e985f07846ff6ade8cf03ba
GradyArmstrong <144718427+GradyArmstrong@users.noreply.github.com>
2024-06-07 18:33:09 +0000

    .

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,17 +1,24 @@
 const cards = document.getElementsByClassName("memory-card");
-      
+
 let selectedCard = null;
+let matchedPairs = 0;
+
 for (const card of cards){
   card.addEventListener("click",()=>{
     card.getElementsByClassName("back")[0].hidden = true;
     if (selectedCard == null){
       selectedCard = card;
     } else if (selectedCard.getAttribute("data-framework") != card.getAttribute("data-framework")){
-      card.getElementsByClassName("back")[0].hidden = false;
-      selectedCard.getElementsByClassName("back")[0].hidden = false;
-      selectedCard = null;
+      setTimeout(() => {
+        card.getElementsByClassName("back")[0].hidden = false;
+        selectedCard.getElementsByClassName("back")[0].hidden = false;
+        selectedCard = null;
+      }, 1000);
     } else {
       selectedCard = null;
+      matchedPairs++;
+      if (matchedPairs === cards.length / 2) {
+      }
     }
-  })
+  });
 }
